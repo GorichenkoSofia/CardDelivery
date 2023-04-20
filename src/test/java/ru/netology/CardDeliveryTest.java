2f9d40c4adb10459c22aabd75bb8635920938ce0
POP CODE <lolo4ka69@list.ru>
2023-04-20 15:54:02 +0300

    Test passed4

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -21,7 +21,7 @@ public class CardDeliveryTest {
     public void shouldBeSuccessfullComplited() {
         open("http://localhost:9999/");
         $x("//span[@data-test-id='city']//input").setValue("Санкт-Петербург");
-        String currentDate = dataGenerate(4, "20.04.2023") ;
+        String currentDate = dataGenerate(3, "dd.MM.yyyy") ;
         $x("//span[@data-test-id='date']//input").sendKeys(Keys.chord(Keys.CONTROL, Keys.SHIFT, Keys.ARROW_LEFT), Keys.DELETE);
         $x("//span[@data-test-id='date']//input").sendKeys(currentDate);
         $x("//span[@data-test-id='name']//input").setValue("Гориченко София");
